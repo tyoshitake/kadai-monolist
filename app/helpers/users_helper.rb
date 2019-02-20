256b40a29ce753b56544f3daa40b2371599349f0
tomoyuki.yoshitake <tyoshitake.dev@gmail.com>
2019-02-20 14:52:56 +0000

    Add want ranking

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@ module UsersHelper
   def gravatar_url(user, options={ size: 80})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    "https://secure.garavatar.com/avatar/#{gravatar_id}?s=#{size}%d=mp"
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}%d=mp"
   end
 end
