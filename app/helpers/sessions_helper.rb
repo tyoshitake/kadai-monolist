fa2e4a96c05b1b5562bfa9e65de5be6d81e0bc25
tomoyuki.yoshitake <tyoshitake.dev@gmail.com>
2019-02-23 05:26:26 +0000

    Modify sessions_helper.rb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
   
   def current_user
-    @user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
   
   def logged_in?
